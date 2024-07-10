2d3c64d82444515220d4109cd03eb5d8dd56ce96
Jepennn <jeppehesselgren@gmail.com>
2024-07-10 10:27:07 +0200

    Restart of js

--- Courses/TraversyMedia-12hCourse/script.js
+++ Courses/TraversyMedia-12hCourse/script.js
@@ -1,23 +1,2 @@
 
-//Challange 3 Arrays
-
-// A)
-const arr = [1,2,3,4,5];
-
-arr.push(6)
-arr.unshift(0)
-arr.reverse()
-
-console.log(arr); // Prints [6,5,4,3,2,1,0]
-
-// B)
-
-const arr1 = [1,2,3,4,5];
-const arr2 = [5,6,7,8,9,10];
-
-const arr3 = [...arr1.slice(0,4), ...arr2] ;
-
-console.log(arr3);
-
-
 
