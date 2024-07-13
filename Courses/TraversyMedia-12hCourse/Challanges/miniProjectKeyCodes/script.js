bd563ec9b0e89c660a0bc124a8793af0842b8337
Jepennn <jeppehesselgren@gmail.com>
2024-07-13 07:43:20 +0200

    Finshed challenge. Two solution

--- Courses/TraversyMedia-12hCourse/Challanges/miniProjectKeyCodes/script.js
+++ Courses/TraversyMedia-12hCourse/Challanges/miniProjectKeyCodes/script.js
@@ -1,3 +1,66 @@
 
-/*Hear are you gone write js code to pressent i key code of 
-  the key you press on the screen*/
+// /*Hear are you gone write js code to pressent i key code of 
+//   the key you press on the screen*/
+
+//   //Method 1
+//   window.addEventListener('keydown', (e) => {
+//     console.log(e);
+  
+//     const keyCode = document.querySelector('.key-code');
+//     const key = document.querySelector('.key');
+//     const code = document.querySelector('.code');
+
+//     const insert = document.getElementById('insert');
+
+//     insert.innerHTML =`
+//         <div class="key">
+//             <small>e.key</small>
+//             ${e.key === ' ' ? 'Space' : e.key}
+//         </div>
+//         <div class="key">
+//             <small>e.keyCode</small>
+//             ${e.keyCode}
+//         </div>
+//         <div class="key">
+//             <small>e.code</small>
+//             ${e.code}
+//         </div>
+//     </div>`;
+// });
+
+  //Method 2
+
+    
+  function keyPressed(e) {
+
+    const allValues = {
+        "e.key": e.key === " " ? "space" : e.key,
+        "e.keyCode": e.keyCode,
+        "e.code": e.code,
+    }
+
+    const insert = document.getElementById('insert');
+    insert.innerHTML = "";
+
+    for(let key in allValues){
+
+        const div = document.createElement("div");
+        const keyValue = document.createTextNode(allValues[key]);
+        div.appendChild(keyValue);
+        div.className = "key";
+
+        const small = document.createElement("small");
+        const keyText = document.createTextNode(key);
+        small.appendChild(keyText)
+
+        div.appendChild(small)
+        insert.appendChild(div)
+    }
+
+  }
+  
+  
+  
+  window.addEventListener('keydown', keyPressed);
+
+
